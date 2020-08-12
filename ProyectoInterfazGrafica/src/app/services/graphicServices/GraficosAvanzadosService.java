3b862af8e5ae2dc44e10f9149d765051454ce769
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-08-12 13:10:39 -0500

    Se elimina control de stroke

--- ProyectoInterfazGrafica/src/app/services/graphicServices/GraficosAvanzadosService.java
+++ ProyectoInterfazGrafica/src/app/services/graphicServices/GraficosAvanzadosService.java
@@ -239,7 +239,6 @@ public class GraficosAvanzadosService {
             public void paintBorder(Component c, Graphics g, int x, int y, int ancho, int alto) {
                 Graphics2D g2= (Graphics2D) g;
                 g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                g2.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_NORMALIZE);
                 Area area;
                 Component padreContenedor  = c.getParent();
                 RoundRectangle2D rectanguloBordeado = new RoundRectangle2D.Double();
@@ -286,7 +285,6 @@ public class GraficosAvanzadosService {
             public void paintBorder(Component c, Graphics g, int x, int y, int ancho, int alto) {
                 Graphics2D g2 = (Graphics2D) g;
                 g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                g2.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_NORMALIZE);
                 Area area;
                 Component padreContenedor  = c.getParent();
                 Ellipse2D circulo = new Ellipse2D.Double();
